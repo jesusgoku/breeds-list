81e8389e4ea71d7a300f10eec9d044394c2d0ed0
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-08-27 11:49:31 -0400

    Added more test to BreedFilterItem component

--- src/components/BreedFilterItem.test.js
+++ src/components/BreedFilterItem.test.js
@@ -1,18 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
 import BreedFilterItem from './BreedFilterItem';
 
-const props = {
+const baseProps = {
   item: {
     name: 'some',
     sub: [],
     path: ['some'],
   },
-  onToggleItem: () => {},
+  onToggleItem: jest.fn(() => {}),
 };
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BreedFilterItem {...props} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('BreedFilterItem', () => {
+  it('renders without crashing', () => {
+    mount(<BreedFilterItem {...baseProps} />);
+  });
+
+  it('renders (shallow) without crashing', () => {
+    shallow(<BreedFilterItem {...baseProps} />);
+  });
+
+  it('should render BreedFilterItem__Label', () => {
+    const wrapper = shallow(<BreedFilterItem {...baseProps} />);
+
+    expect(wrapper.find('.BreedFilterItem__Label').length).toEqual(1);
+  });
+
+  it('should render recursive component', () => {
+    const props = {
+      ...baseProps,
+      item: {
+        ...baseProps.item,
+        sub: [baseProps.item]
+      }
+    };
+
+    const wrapper = mount(<BreedFilterItem {...props} />);
+
+    expect(wrapper.find('.BreedFilterItem').length).toEqual(2);
+  });
+
+  it('should call onToggleItem', () => {
+    const wrapper = shallow(<BreedFilterItem {...baseProps} />);
+
+    wrapper.find('.BreedFilterItem__Input').simulate('change', { target: { checked: true } })
+
+    expect(baseProps.onToggleItem.mock.calls.length).toEqual(1);
+  });
 });
