f6def27a1f8fb02a96fe0ab945b46e9864b47713
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-08-27 01:21:03 -0400

    Make App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import BreedPills from './components/BreedPills';
+import BreedFilter from './components/BreedFilter';
+import Gallery from './components/Gallery';
+
+import BreedsApi from './services/breeds-api';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      breedsList: [],
+      selected: {},
+    };
+
+    this.handleToggleItem = this.handleToggleItem.bind(this);
+    this.handleClearItems = this.handleClearItems.bind(this);
+  }
+
+  async componentDidMount() {
+    const breedsList = await BreedsApi.getBreedsList();
+    this.setState({ breedsList });
+  }
+
+  handleToggleItem(item) {
+    const { name } = item;
+
+    this.setState(({ selected }) => {
+      if (name in selected) {
+        const { [name]: _, ...rest } = selected;
+        return { selected: rest };
+      } else {
+        return { selected: {...selected, [name]: item} };
+      }
+    });
+  }
+
+  handleClearItems() {
+    this.setState({ selected: {} });
+  }
+
+  processList(list) {
+    const { selected } = this.state;
+
+    return list
+      .map(({ name, sub, ...rest }) => ({
+        ...rest,
+        name,
+        selected: name in selected,
+        sub: sub && sub.length ? this.processList(sub) : [],
+      }));
+  }
+
+  render() {
+    const { breedsList, selected } = this.state;
+    const items = this.processList(breedsList);
+
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 className="text-center">Breeds List</h1>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4">
+              <div className="BreedSidebar">
+                <BreedPills items={selected} onToggleItem={this.handleToggleItem} onClearItems={this.handleClearItems} />
+                <BreedFilter items={items} onToggleItem={this.handleToggleItem} />
+              </div>
+            </div>
+
+            <div className="col-md-8">
+              <Gallery items={selected} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
